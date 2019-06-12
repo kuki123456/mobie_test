3abcd31c5af4d6f461c7d63af63010d8b9e1db0c
kuki123456 <2278634411@qq.com>
2019-06-12 15:49:17 +0800

    dafas

--- base/base_build_data.py
+++
@@ -1,8 +0,0 @@
-import yaml
-def build_data(data_key,filename):
-    # ls_data=[]
-    with open("data/"+filename,"r",encoding="utf-8") as f:
-        data=yaml.load(f)
-        # for i in data[data_key].values():
-        #     ls_data.append(i)
-        return data[data_key].values()
